bf1353b0dd6e7b368a27ca70845b07e56a56e278
HARUN KOR <harunkor@HARUN-MacBook-Pro.local>
2017-08-08 00:23:27 +0300

    Initial commit

---
+++ app/src/main/java/tr/com/harunkor/googleplayservicesimplesampleandroidstudio/MainActivity.java
@@ -0,0 +1,95 @@
+package tr.com.harunkor.googleplayservicesimplesampleandroidstudio;
+
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.gms.games.Games;
+import com.google.example.games.basegameutils.BaseGameActivity;
+
+
+public class MainActivity extends BaseGameActivity {
+    private EditText edt_skor;
+    private Button   btn_kayit;
+    private Button   btn_skorlar;
+    private Button   btn_rozetler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        edt_skor=(EditText)findViewById(R.id.edttxt_Score_Input);
+        btn_kayit=(Button)findViewById(R.id.SaveScore);
+        btn_skorlar=(Button)findViewById(R.id.Leaderboards);
+        btn_rozetler=(Button)findViewById(R.id.badges);
+
+        beginUserInitiatedSignIn();
+
+        btn_kayit.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                int score = Integer.parseInt(edt_skor.getText().toString());
+                Games.Achievements.unlock(getApiClient(),
+                        getString(R.string.correct_guess_achievement_badge1));
+                Games.Achievements.unlock(getApiClient(),
+                        getString(R.string.correct_guess_achievement_badge2));
+                Games.Achievements.unlock(getApiClient(),
+                        getString(R.string.correct_guess_achievement_badge3));
+                Games.Achievements.unlock(getApiClient(),
+                        getString(R.string.correct_guess_achievement_badge4));
+
+
+
+
+                Games.Leaderboards.submitScore(getApiClient(),
+                        getString(R.string.number_guesses_leaderboard),
+                        score);
+
+            }
+        });
+
+        btn_skorlar.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                startActivityForResult(Games.Leaderboards.getLeaderboardIntent(
+                        getApiClient(), getString(R.string.number_guesses_leaderboard)),
+                        2);
+            }
+        });
+
+
+        btn_rozetler.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                startActivityForResult(Games.Achievements.getAchievementsIntent(
+                        getApiClient()), 1);
+            }
+        });
+
+
+
+    }
+
+    @Override
+    public void onSignInFailed() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void onSignInSucceeded() {
+        // TODO Auto-generated method stub
+
+    }
+
+
+}
